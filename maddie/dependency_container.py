d9a18e0dabae72d9bb9333443e219af5565d93df
nadavgu <nadavgu@gmail.com>
2023-08-08 21:13:02 +0300

    Allowing tags of all types

--- maddie/dependency_container.py
+++ maddie/dependency_container.py
@@ -1,9 +1,8 @@
 from typing import Any, Dict, TypeVar, Type, Callable, Tuple, Optional
 
 T = TypeVar('T')
-
-
-DependencyID = Tuple[Type, Optional[str]]
+Tag = Any
+DependencyID = Tuple[Type, Tag]
 
 
 class DependencyContainer:
@@ -11,25 +10,25 @@ class DependencyContainer:
         self.__dependencies: Dict[DependencyID, Any] = {}
         self.__providers: Dict[DependencyID, Callable[['DependencyContainer'], Any]] = {}
 
-    def add(self, dependency_type: Type[T], dependency: T, tag: Optional[str] = None):
+    def add(self, dependency_type: Type[T], dependency: T, tag: Tag = None):
         self.__dependencies[(dependency_type, tag)] = dependency
 
-    def add_dependency(self, dependency: T, tag: Optional[str] = None):
+    def add_dependency(self, dependency: T, tag: Tag = None):
         self.add(type(dependency), dependency, tag)
 
-    def get(self, dependency_type: Type[T], tag: Optional[str] = None) -> T:
+    def get(self, dependency_type: Type[T], tag: Tag = None) -> T:
         if (dependency_type, tag) not in self.__dependencies:
             self.add(dependency_type, self.create(dependency_type, tag), tag)
         return self.__dependencies[(dependency_type, tag)]
 
-    def create(self, dependency_type: Type[T],  tag: Optional[str] = None) -> T:
+    def create(self, dependency_type: Type[T],  tag: Tag = None) -> T:
         if (dependency_type, tag) in self.__providers:
             return self.__providers[(dependency_type, tag)](self)
         return dependency_type.create(self)
 
     def add_provider(self, dependency_type: Type[T], provider: Callable[['DependencyContainer'], Any],
-                     tag: Optional[str] = None):
+                     tag: Tag = None):
         self.__providers[(dependency_type, tag)] = provider
 
-    def bind(self, dependency_type: Type[T], providing_type: Type[T], tag: Optional[str] = None):
+    def bind(self, dependency_type: Type[T], providing_type: Type[T], tag: Tag = None):
         self.add_provider(dependency_type, lambda dependency_container: dependency_container.get(providing_type), tag)
